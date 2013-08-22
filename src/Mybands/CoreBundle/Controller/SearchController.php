b3f78f3b265f204c6883580ddd3f711bdc30335c
Perraud Cyril <perraud.cyril@gmail.com>
2013-08-22 08:10:39 +0200

    Ajout du controller search et des vues associées
    Change-Id: I37f896d5b9395b9426ea1d28384daac5f5773fd8

---
+++ src/Mybands/CoreBundle/Controller/SearchController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace MyBands\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+// these import the "@Route" and "@Template" annotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class SearchController extends Controller
+{
+    /**
+     * @Route("/search/{searchValue}", name="_homeSearch")
+     * @Template()
+     */
+    public function indexAction($searchValue)
+    {
+        $data = array();
+        $data['title'] = 'Recherche - Site de promotion musicale';
+        $data['searchValue'] = $searchValue;
+        if ($this->getRequest()->isMethod('POST')) {
+            $data['content'] = $this->renderView('MyBandsCoreBundle:Search:content.html.twig', $this->contentAction($searchValue));
+            $response = new Response(json_encode($data));
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;
+        }
+        return $data;
+    }
+    
+    /**
+     * @Template()
+     */
+    public function contentAction($searchValue)
+    {
+    	$data = array();
+        $data['searchValue'] = $searchValue;
+        return $data;
+    }
+    
+}
