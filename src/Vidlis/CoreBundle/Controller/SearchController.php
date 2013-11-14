924cdb40f7a8312a3cd6990379ddf9218d401484
cyril <perraud.cyril@gmail.com>
2013-11-14 20:36:38 +0100

    Gestion des playlists

--- src/Vidlis/CoreBundle/Controller/SearchController.php
+++ src/Vidlis/CoreBundle/Controller/SearchController.php
@@ -21,7 +21,7 @@ class SearchController extends AuthController
     public function indexAction($searchValue)
     {
         $data = array();
-        $data['title'] = 'Recherche';
+        $data['title'] = 'Recherche '.$searchValue;
         $data['searchValue'] = $searchValue;
         if ($this->getRequest()->isMethod('POST')) {
             $data['content'] = $this->renderView('VidlisCoreBundle:Search:content.html.twig', $this->contentAction($searchValue));
