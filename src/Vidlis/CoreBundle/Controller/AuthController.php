4d61864a05e2b7d9b759aab379a8581ec0af77fd
cyril <perraud.cyril@gmail.com>
2015-03-10 20:21:00 +0100

    Add login action for mobile application

--- src/Vidlis/CoreBundle/Controller/AuthController.php
+++ src/Vidlis/CoreBundle/Controller/AuthController.php
@@ -1,11 +1,15 @@
 <?php
 namespace Vidlis\CoreBundle\Controller;
 
+use FOS\UserBundle\FOSUserBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Vidlis\CoreBundle\GoogleApi\Client;
-
+use Vidlis\UserBundle\Entity\User;
 
 /**
  * Class AuthController
@@ -50,7 +54,8 @@ class AuthController extends Controller
     /**
      * Init YoutubeAuthentication Token
      */
-    public function initialize() {
+    public function initialize()
+    {
         $OAUTH2_CLIENT_ID = $this->container->getParameter('OAUTH2_CLIENT_ID');
         $OAUTH2_CLIENT_SECRET = $this->container->getParameter('OAUTH2_CLIENT_SECRET');
         $this->client = new Client();
@@ -61,4 +66,80 @@ class AuthController extends Controller
           FILTER_SANITIZE_URL);
         $this->client->setRedirectUri($redirect);
     }
+
+
+    /**
+     * @Route("/loginRemote", name="_logInRemote")
+     */
+    public function logInRemoteAction()
+    {
+        $status = true;
+        $message = ['error'];
+        $request = $this->getRequest();
+        $username = $request->get('username');
+        $password = $request->get('password');
+
+        $user = $this->getDoctrine()
+            ->getManager()
+            ->getRepository("VidlisUserBundle:User")
+            ->findOneByUsername($username);
+
+        if(!$user){
+            $user = $this->getDoctrine()
+                ->getManager()
+                ->getRepository("VidlisUserBundle:User")
+                ->findOneByEmail($username);
+        }
+
+        if(!$user instanceof User){ // User not found
+            $message['status'] = 'FAIL';
+            $message['error'] = 'Utilisateur non trouvé';
+            $status = false;
+        }
+
+        if(!$this->checkUserPassword($user, $password)){ // Wrong password
+            $message['status'] = 'FAIL';
+            $message['error'] = 'Mauvais mot de passe';
+            $status = false;
+        }
+
+        $this->loginUser($user);
+        $response = new Response(
+            json_encode(['status' => $status, 'login' => $user->getUsername(), $message['error']]),
+            201,
+            [
+                'Access-Control-Allow-Origin' => '*',
+                'Content-Type' => 'application/json'
+            ]);
+        return $response;
+    }
+
+    protected function loginUser(User $user)
+    {
+        $security = $this->get('security.context');
+        $providerKey = $this->container->getParameter('fos_user.firewall_name');
+        $roles = $user->getRoles();
+        $token = new UsernamePasswordToken($user, null, $providerKey, $roles);
+        $security->setToken($token);
+    }
+
+    protected function logoutUser()
+    {
+        $security = $this->get('security.context');
+        $token = new AnonymousToken(null, new User());
+        $security->setToken($token);
+        $this->get('session')->invalidate();
+    }
+
+    protected function checkUserPassword(User $user, $password)
+    {
+        $factory = $this->get('security.encoder_factory');
+        $encoder = $factory->getEncoder($user);
+        if(!$encoder){
+            return false;
+        }
+        return $encoder->isPasswordValid($user->getPassword(), $password, $user->getSalt());
+    }
+
+
 }
