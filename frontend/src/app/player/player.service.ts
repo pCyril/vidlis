ec7438b2edbbf8d29b428bfef8b1b42cf532b867
Cyril <cyril@nexenture.fr>
2018-10-15 21:59:10 +0200

    Fix url

--- frontend/src/app/player/player.service.ts
+++ frontend/src/app/player/player.service.ts
@@ -64,6 +64,6 @@ export class PlayerService {
         });
 
         let body = JSON.stringify({name, ids});
-        return this.http.post(`http://localhost:8080/api/playlists`, body);
+        return this.http.post(`${parameters.apiUrl}/api/playlists`, body);
     }
 }
