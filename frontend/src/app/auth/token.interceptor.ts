2bcd4c620164b7f59eb063be4b037a2c400ed92b
cyrilperraud <cyril@nexenture.fr>
2018-11-05 13:31:02 +0100

    Fix token interceptor

--- frontend/src/app/auth/token.interceptor.ts
+++ frontend/src/app/auth/token.interceptor.ts
@@ -7,9 +7,16 @@ import {
 } from '@angular/common/http';
 import { AuthService } from './auth/auth.service';
 import { Observable } from 'rxjs/Observable';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
+
+
+    constructor(
+        public router: Router
+    ) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -23,6 +30,14 @@ export class TokenInterceptor implements HttpInterceptor {
             });
         }
 
-        return next.handle(request);
+        return next.handle(request).pipe(
+            catchError( (err) => {
+                if (err.status === 401) {
+                    window.localStorage.removeItem('token');
+                    this.router.navigate(['/']);
+                }
+                return Observable.throw(err);
+            })
+        )
     }
 }
